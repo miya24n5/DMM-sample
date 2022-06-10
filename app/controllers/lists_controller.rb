b7a62cd113f95f7cd8c1b8af8a92637f705d9027
miya24n5 <sho.sakulai10@gmail.com>
2022-06-10 08:19:20 +0000

    5章

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,7 +2,7 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
     list = List.new(list_params)
     list.save
@@ -18,12 +18,19 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
-  
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
   private
-  
+
   def list_params
     params.require(:list).permit(:title,:body)
   end
-  
+
 end
